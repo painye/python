1b258a73730c35abaaa6aa93d02d49e47106da16
painye <1850384745@qq.com>
2019-07-31 12:56:39 +0800

    Create selenium3.py
    selenium的用法3

---
+++ selenium3.py
@@ -0,0 +1,12 @@
+import time
+from selenium import webdriver
+
+browser = webdriver.Edge()
+browser.get('https://www.taobao.com')
+browser.execute_script('window.open()')   # 新建一个选项卡
+print(browser.window_handles)
+browser.switch_to.window(browser.window_handles[1])  # 切换到第二个选项卡
+browser.get('https://www.taobao.com')
+time.sleep(1)
+browser.switch_to.window(browser.window_handles[0]) # 切换到第二个选项卡
+browser.get('https://www.baidu.com')
